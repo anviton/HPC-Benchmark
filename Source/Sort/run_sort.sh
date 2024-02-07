9f07d0571ecf5d8bd88de6793420e94704e86099
reynaldesgr <100307961+reynaldesgr@users.noreply.github.com>
2024-02-07 10:08:40 +0100

    Add sort files for Erlang and Rust

--- Source/Sort/run_sort.sh
+++ Source/Sort/run_sort.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-path="./"
+base_path="./"
 
+cd "${base_path}C/sort"
+gcc bubble-sort.c -o bubble-sort_c
+echo "[Bubble sort] - run C file..."
+time ./bubble-sort_c
+cd - > /dev/null
 
-cd "${base_path}python/sort"
+cd "${base_path}Python/sort"
 echo "[Bubble sort] - run Python file..."
 time python3 bubble-sort.py
+cd - > /dev/null
+
+
+cd "${base_path}Rust/sort"
+rustc bubble-sort.rs -o bubble-sort_rust
+echo "[Bubble sort] - run Rust file..."
+time ./bubble-sort_rust
+cd - > /dev/null
+
+cd "${base_path}Erlang/sort"
+erlc bubble_sort.erl
+echo "[Bubble sort] - run Erlang file..."
+time erl -noshell -s bubble_sort main -s init stop
 cd - > /dev/null
