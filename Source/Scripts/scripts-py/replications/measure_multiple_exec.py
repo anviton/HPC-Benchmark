a23589b91b4122630cc0afc203fe7854b5d35362
reynaldesgr <reynalde.segerie.deveze@gmail.com>
2024-03-01 17:45:28 +0100

    Fix .py for measures

--- Source/Scripts/scripts-py/replications/measure_multiple_exec.py
+++ Source/Scripts/scripts-py/replications/measure_multiple_exec.py
@@ -84,24 +84,24 @@ def execute_and_measure(executable_name, args, language):
 
     if language == "java":
         class_name = os.path.splitext(os.path.basename(source_file))[0]
-        command = ["taskset", "-c", cpu_mask, "/usr/bin/time", "-f", "%U %S %e", "java", "-cp", bin_dir, class_name]
+        command = ["/usr/bin/time", "-f", "%U %S %e", "taskset", "-c", cpu_mask, "java", "-cp", bin_dir, class_name]
     elif language in ["ruby"]:
-        command = ["taskset", "-c", cpu_mask, "/usr/bin/time", "-f", "%U %S %e", language, source_file]
+        command = ["/usr/bin/time", "-f", "%U %S %e", "taskset", "-c", cpu_mask, language, source_file]
     elif language in ["python"]:
-        command = ["taskset", "-c", cpu_mask, "/usr/bin/time", "-f", "%U %S %e", "python3", source_file]
+        command = ["/usr/bin/time", "-f", "%U %S %e", "taskset", "-c", cpu_mask, "python3", source_file]
     elif language == "erlang":
         if executable_args:
             erl_command = f'erl -noshell -s {executable_name} main {executable_args} -s init stop'
         else:
             erl_command = f'erl -noshell -s {executable_name} main -s init stop'
-        command_str = f'taskset -c {cpu_mask} /usr/bin/time -f "%U %S %e" {erl_command}'
+        command_str = f'/usr/bin/time -f "%U %S %e" taskset -c {cpu_mask} {erl_command}'
         result = subprocess.run(command_str, capture_output=True, text=True, shell=True)
         if result.returncode != 0:
             erl_command = f'erl -noshell -run {executable_name} main {executable_args}'
-            command_str = f'taskset -c {cpu_mask} /usr/bin/time -f "%U %S %e" {erl_command}'
+            command_str = f'/usr/bin/time -f "%U %S %e" taskset -c {cpu_mask} {erl_command}'
             result = subprocess.run(command_str, capture_output=True, text=True, shell=True)
     else:
-        command = ["taskset", "-c", cpu_mask, "/usr/bin/time", "-f", "%U %S %e", "./" + executable_name]
+        command = ["/usr/bin/time", "-f", "%U %S %e", "taskset", "-c", cpu_mask, "./" + executable_name]
 
     if language != "erlang":
         if executable_args:
@@ -112,7 +112,7 @@ def execute_and_measure(executable_name, args, language):
         print("Execution error:", result.stderr)
         return None
     
-    print(command.args)
+    print(command)
 
     timings = result.stderr.strip().split()
     user_time, sys_time, real_time = timings[0], timings[1], timings[2]
